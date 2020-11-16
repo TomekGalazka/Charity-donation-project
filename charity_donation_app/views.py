f1a09581e825a9ad9440a7e4ff8f52c40bbce52b
Tomasz Gałązka <tom3k.galazka@gmail.com>
2020-11-16 15:16:49 +0100

    Add anchors to sections in landing page for all templates. Improved performance for displaying supported institutions by counting them on the database side.

--- charity_donation_app/views.py
+++ charity_donation_app/views.py
@@ -8,12 +8,13 @@ class LandingPageView(View):
     def get(self, request, *args, **kwargs):
         all_donations = Donation.objects.all()
         quantity_counter = 0
-        institutions_list = []
+        # institutions_list = []
         for donation in all_donations:
             quantity_counter += donation.quantity
-            if donation.institution not in institutions_list:
-                institutions_list.append(donation.institution)
-        institutions = len(institutions_list)
+        institutions = all_donations.distinct('institution').count()
+        #     if donation.institution not in institutions_list:
+        #         institutions_list.append(donation.institution)
+        # institutions = len(institutions_list)
 
         context = {
             'quantity_counter': quantity_counter,
