51d791cfd720b497e7f5e35a40bbfae234fcf345
Tomasz Gałązka <tom3k.galazka@gmail.com>
2020-11-18 22:02:00 +0100

    Fix new user model. Add dynamic display of institutions to landing page template.

--- charity_donation_app/views.py
+++ charity_donation_app/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render
 from django.views import View
 
-from . models import Donation
+from . models import Donation, Institution, Category
 
 
 class LandingPageView(View):
     def get(self, request, *args, **kwargs):
         all_donations = Donation.objects.all()
         quantity_counter = 0
-        # institutions_list = []
         for donation in all_donations:
             quantity_counter += donation.quantity
         institutions = all_donations.distinct('institution').count()
-        #     if donation.institution not in institutions_list:
-        #         institutions_list.append(donation.institution)
-        # institutions = len(institutions_list)
+
+        charitable_trust_institutions = Institution.objects.filter(type='Trust')
+        non_gov_institutions = Institution.objects.filter(type='Non-gov')
+        local_institutions = Institution.objects.filter(type='Local')
+
+        list_1 = []
+
+        for institution in charitable_trust_institutions:
+            for category in institution.categories.values_list():
+                list_1.append(category[1])
+                institution_categories = ", ".join(list_1)
 
         context = {
             'quantity_counter': quantity_counter,
-            'institution_counter': institutions
+            'institution_counter': institutions,
+            'Found': charitable_trust_institutions,
+            'Non_gov': non_gov_institutions,
+            'Local': local_institutions,
         }
         return render(request, 'index.html', context)
 
