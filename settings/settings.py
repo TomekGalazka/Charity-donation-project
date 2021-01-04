1be7d88157b5212af2488f4a0f11ff4f9a4e1ad3
Tomasz Gałązka <tom3k.galazka@gmail.com>
2021-01-04 22:27:44 +0100

    Security fix.

--- settings/settings.py
+++ settings/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-from .secret_settings import *
+try:
+    from .secret_settings import *
+except ImportError: print("Error: make a local version of private_settings.py from the template")
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
